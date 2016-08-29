95894242d7cc9344d283819510bd1be14376fe10
Nerevar <gilet666@hotmail.fr>
2016-08-29 17:19:16 +0200

    bootstrap update & media test

---
+++ Quotes/Controllers/QuoteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quotes.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyQuotes()
+        {
+            return View();
+        }
+    }
+}
