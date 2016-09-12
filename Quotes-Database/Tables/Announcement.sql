678e4c2c4ffd3486218b76dc11f64a17444a5b17
Nerevar <gilet666@hotmail.fr>
2016-09-12 18:09:48 +0200

    anouncement table

---
+++ Quotes-Database/Tables/Announcement.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Announcement]
+(
+	[AnnId] INT NOT NULL PRIMARY KEY, 
+	[AnnTitle] NVARCHAR(256) NULL,
+    [AnnRawHtmlBody] NVARCHAR(MAX) NULL,
+	[AnnCreationDate] DATETIME2 NULL, 
+    [AnnUpdateDate] DATETIME2 NULL, 
+    [AnnModifiedById] NCHAR(10) NULL, 
+)
